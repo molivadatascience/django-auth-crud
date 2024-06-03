0dc825aeda3a388a84fd7faf466719628c4328ef
molivadatascience <molivaromero@gmail.com>
2024-06-02 23:06:50 -0400

    ajustes 6

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,7 +7,7 @@ class TaskForm(forms.ModelForm):
         fields=['nombre_cliente','fecha_solicitud', 'observaciones', 'important',] #son los campos que quiero ver en el formulario el resto no es necesario
         widgets = {
             'nombre_cliente': forms.TextInput(attrs={'class': 'form-control','placeholder': 'nombre_cliente'}),
-            'fecha_solicitud': forms.DateField(attrs={'class': 'form-control','placeholder': 'fecha_solicitud'}),
+            'fecha_solicitud': forms.DateTimeField(attrs={'class': 'form-control','placeholder': 'fecha_solicitud'}),
             'observaciones': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Observaciones'}),
             'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
         }
