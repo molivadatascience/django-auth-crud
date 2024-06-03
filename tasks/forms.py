4bc97708528fe9806424efc4739d54606606c769
molivadatascience <molivaromero@gmail.com>
2024-06-03 00:12:59 -0400

    ajustes 22

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,7 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields=['nombre_cliente', 'campaña','observaciones', 'important',] #son los campos que quiero ver en el formulario el resto no es necesario
+        fields=['nombre_cliente', 'campaña','observaciones', 'important'] #son los campos que quiero ver en el formulario el resto no es necesario
         widgets = {
             'nombre_cliente': forms.TextInput(attrs={'class': 'form-control','placeholder': 'nombre_cliente'}),
             'campaña': forms.TextInput(attrs={'class': 'form-control','placeholder': 'campaña'}),
