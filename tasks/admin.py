e441b7539435debc5590a0931b8f04ca712bfb08
molivadatascience <molivaromero@gmail.com>
2024-06-03 00:56:26 -0400

    ajustes 2

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Task #Para traer el modelo llamado tarea
 
 class TaskAdmin(admin.ModelAdmin):
-    readonly_fields = ("created", )
+    readonly_fields = ("fecha_solicitud", )
 
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
