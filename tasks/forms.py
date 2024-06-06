a6f03dba242a65154166b4f8f8bb9e9d2137c31b
molivadatascience <molivaromero@gmail.com>
2024-06-06 17:54:39 -0400

    actualizando_formularios_9

--- tasks/forms.py
+++ tasks/forms.py
@@ -11,7 +11,7 @@ class TaskForm(forms.ModelForm):
             'nombre_cliente':forms.TextInput(attrs={'class': 'form-control','placeholder': 'nombre_cliente'}),
             'campana': forms.TextInput(attrs={'class': 'form-control','placeholder': 'campana'}),
             'kam':forms.TextInput(attrs={'class': 'form-control','placeholder': 'kam'}),
-            'valor_oportunidad':forms.IntegerField(attrs={'class': 'form-control'}),
+            'valor_oportunidad':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Ingresa valor'}),
             'tipo_importacion':forms.TextInput(attrs={'class': 'form-control','placeholder': 'tipo_importacion'}),
             'pais_origen':forms.TextInput(attrs={'class': 'form-control','placeholder': 'pais_origen'}),
             'fecha_entrega_propuesta':forms.DateInput(attrs={'type': 'date'}),
