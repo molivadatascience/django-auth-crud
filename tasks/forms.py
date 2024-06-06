66fc0f76ac8eb679955cf68873afd1818d18b701
molivadatascience <molivaromero@gmail.com>
2024-06-06 14:14:15 -0400

    actualizando_formularios

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,10 +4,22 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields=['title', 'campana', 'description', 'important'] #son los campos que quiero ver en el formulario el resto no es necesario
+        fields=['title', 'fecha_solicitud', 'nombre_cliente', 'campana', 'kam', 'tipo_importacion', 'pais_origen', 'fecha_entrega_propuesta', 'fecha_entrega_productos', 'solicitud_muestra', 'description', 'fecha_cotizacion', 'cotizacion_aceptada', 'nombre_contacto', 'email_contacto', 'important'] #son los campos que quiero ver en el formulario el resto no es necesario
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Write a title'}),
+            'fecha_solicitud':forms.DateField(attrs={'class': 'form-control'}),
+            'nombre_cliente':forms.TextInput(attrs={'class': 'form-control','placeholder': 'nombre_cliente'}),
             'campana': forms.TextInput(attrs={'class': 'form-control','placeholder': 'campana'}),
+            'kam':forms.TextInput(attrs={'class': 'form-control','placeholder': 'kam'}),
+            'tipo_importacion':forms.TextInput(attrs={'class': 'form-control','placeholder': 'tipo_importacion'}),
+            'pais_origen':forms.TextInput(attrs={'class': 'form-control','placeholder': 'pais_origen'}),
+            'fecha_entrega_propuesta':forms.DateInput(attrs={'class': 'form-control'}),
+            'fecha_entrega_productos':forms.DateInput(attrs={'class': 'form-control'}),
+            'solicitud_muestra': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
             'description': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Write a description'}),
+            'fecha_cotizacion':forms.DateInput(attrs={'class': 'form-control'}),
+            'cotizacion_aceptada':forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
+            'nombre_contacto': forms.TextInput(attrs={'class': 'form-control','placeholder': 'nombre_contacto'}),
+            'email_contacto': forms.TextInput(attrs={'class': 'form-control','placeholder': 'email_contacto'}),
             'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
         }
--- tasks/models.py
+++ tasks/models.py
@@ -6,8 +6,20 @@ from django.contrib.auth.models import User
 # from tkinter import CASCADE
 class Task(models.Model):
     title = models.CharField(max_length=100)
+    fecha_solicitud = models.DateTimeField(auto_now_add=True)
+    nombre_cliente = models.CharField(max_length=100)
     campana = models.CharField(max_length=100)
+    kam = models.CharField(max_length=100)
+    tipo_importacion= models.CharField(max_length=100)
+    pais_origen = models.CharField(max_length=100)
+    fecha_entrega_propuesta = models.DateTimeField(null=True, blank=True)
+    fecha_entrega_productos = models.DateTimeField(null=True, blank=True)
+    solicitud_muestra = models.BooleanField(default=False)
     description = models.TextField(blank=True)
+    fecha_cotizacion = models.DateTimeField(null=True, blank=True)
+    cotizacion_aceptada = models.BooleanField(default=False)
+    nombre_contacto = models.CharField(max_length=100)
+    email_contacto = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True) # el automaticamente guarda la fecha
     datecompleted = models.DateTimeField(null=True, blank=True) #permite valores nulos
     important = models.BooleanField(default=False)
