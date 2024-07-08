949244eecda7e2eedaa9f67daaed413872cb06ba
molivadatascience <molivaromero@gmail.com>
2024-07-08 18:41:12 -0400

    actualizando_formularios

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -47,8 +47,8 @@ urlpatterns = [
     path('create_task/', views.create_task, name='create_task'),
     path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
     path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
-    #path('create_costeo/', views.create_costeo, name='create_costeo'),
-    path('create_costeo/<int:detalle_oportunidad_id>/', views.create_costeo, name='create_costeo'),
+    path('create_costeo/', views.create_costeo, name='create_costeo'),
+    #path('create_costeo/<int:detalle_oportunidad_id>/', views.create_costeo, name='create_costeo'),
     path('edit_costeo/<int:costeo_id>/', views.edit_costeo, name='edit_costeo'),
     path('delete_costeo/<int:costeo_id>/', views.delete_costeo, name='delete_costeo'),
 ]
